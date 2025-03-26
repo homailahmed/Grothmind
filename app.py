df2433cdbc002df46cff35cf7acefa1b67e0225c
homailahmed <homailsunflowerind@gmail.com>
2025-03-26 12:10:02 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+# Imports
+import streamlit as st
+import  pandas as pd
+import  os 
+from io import BytesIO
+
+
+# Set up our App
+st.set_page_config(page_title="Data sweeper", layout="wide")
+st.title("data sweep")
+st.write("Trasfrom your files between CSV and excel fromats with built-in dtat cleaning and visualization")
+         
+         
